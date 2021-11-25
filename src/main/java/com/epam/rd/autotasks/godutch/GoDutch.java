dc550199188f1dc115b05caa84725b1a6774aa3c
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-25 13:12:49 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,23 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner scanner = new Scanner(System.in);
+        int totalSum = scanner.nextInt();
+        int countFriends = scanner.nextInt();
+
+        if (totalSum < 0){
+            System.out.println("Bill total amount cannot be negative");
+        } else if (countFriends <= 0){
+            System.out.println("Number of friends cannot be negative or zero");
+        } else {
+            totalSum = (int) (totalSum * 1.1);
+            totalSum = totalSum / countFriends;
+            System.out.println(totalSum);
+        }
     }
 }
